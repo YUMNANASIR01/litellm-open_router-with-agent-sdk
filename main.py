b350acd9300ce2712c1ceacc1291eba8969ebf77
YUMNANASIR01 <yumnanasir719@gmail.com>
2025-06-11 22:20:52 +0500

    up

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
+
 import os
-from dotenv import load_dotenv 
-from agents import Agent,Runner,OpenAIChatCompletionsModel,AsyncOpenAI,set_tracing_disabled
+from dotenv import load_dotenv
+from agents import Agent, Runner, set_tracing_disabled 
+from agents.extensions.models.litellm_model import LitellmModel 
+import litellm
+
+litellm.disable_aiohttp_transport = True
 
 load_dotenv()
 set_tracing_disabled(disabled=True)
-# ------------------------------------------------------------------
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
-if not OPENROUTER_API_KEY:
-    raise Exception("OPENROUTER_API_KEY is not set .")
-
-# ------------------------------------------------------------------
-
-client = AsyncOpenAI(
-    api_key = OPENROUTER_API_KEY,
-    base_url = "https://openrouter.ai/api/v1",
-)
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MODEL = "gemini/gemini-1.5-flash"
 
 agent = Agent(
-    model = OpenAIChatCompletionsModel(model="deepseek/deepseek-chat-v3-0324:free", openai_client=client),
-    name = "yumna_agent",
-    instructions = "you are a helpful assistant",
+    name="my_agent",
+    instructions="You are a helpful assistant",
+    model=LitellmModel(model=MODEL, api_key=GEMINI_API_KEY),
 )
-
-res = Runner.run_sync(agent, "I am yumna , Who are you ?")
+res = Runner.run_sync(agent, "I am Yumna, Who are You?")
 print(res.final_output)
